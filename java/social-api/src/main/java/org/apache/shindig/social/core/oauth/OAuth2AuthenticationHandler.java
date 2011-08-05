c8ab27af4079ebb92af6c52a66afd37c02170529
mgmarum <mgmarum@gmail.com>
2011-08-05 14:52:02 -0400

    test 2

--- java/social-api/src/main/java/org/apache/shindig/social/core/oauth/OAuth2AuthenticationHandler.java
+++ java/social-api/src/main/java/org/apache/shindig/social/core/oauth/OAuth2AuthenticationHandler.java
@@ -1,6 +1,11 @@
 package org.apache.shindig.social.core.oauth;
 
-/** COMMENT THESE CLASSES, MATT GAWWWD **/
+/** 
+ * COMMENT THESE CLASSES, MATT GAWWWD 
+ * 
+ * I WILL ALRIGHT!
+ * 
+ * **/
 public class OAuth2AuthenticationHandler {
 
 }
