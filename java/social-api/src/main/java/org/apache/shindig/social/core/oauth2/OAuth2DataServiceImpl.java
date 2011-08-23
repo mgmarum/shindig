e55482f5a714a463dc62f54e5b282eded455896b
woodstae <woodstae@gmail.com>
2011-08-23 14:32:46 -0400

    Implicit client implemented.

--- java/social-api/src/main/java/org/apache/shindig/social/core/oauth2/OAuth2DataServiceImpl.java
+++ java/social-api/src/main/java/org/apache/shindig/social/core/oauth2/OAuth2DataServiceImpl.java
@@ -1,5 +1,12 @@
 package org.apache.shindig.social.core.oauth2;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.io.IOUtils;
 import org.apache.shindig.common.util.ResourceLoader;
 import org.apache.shindig.protocol.ProtocolException;
@@ -8,13 +15,6 @@ import org.apache.shindig.social.core.oauth2.OAuth2Client.ClientType;
 import org.apache.shindig.social.core.oauth2.OAuth2Types.CodeType;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import javax.servlet.http.HttpServletResponse;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
@@ -152,22 +152,25 @@ public class OAuth2DataServiceImpl implements OAuth2DataService {
         OAuth2Client client = converter.convertToObject(clientJson.toString(), OAuth2Client.class);
         client.setType(clientJson.getString("type").equals("public") ? ClientType.PUBLIC : ClientType.CONFIDENTIAL);
         clients.add(client);
-        JSONObject authCodes = oauthDB.getJSONObject(clientId).getJSONObject("authorizationCodes");
-        for(String authCodeId : JSONObject.getNames(authCodes)){
-          OAuth2Code code = converter.convertToObject(authCodes.getJSONObject(authCodeId).toString(), OAuth2Code.class);
-          code.setValue(authCodeId);
-          code.setClient(client);
-          registerAuthorizationCode(clientId, code);
+        if (oauthDB.getJSONObject(clientId).has("authorizationCodes")) {
+          JSONObject authCodes = oauthDB.getJSONObject(clientId).getJSONObject("authorizationCodes");
+          for(String authCodeId : JSONObject.getNames(authCodes)){
+            OAuth2Code code = converter.convertToObject(authCodes.getJSONObject(authCodeId).toString(), OAuth2Code.class);
+            code.setValue(authCodeId);
+            code.setClient(client);
+            registerAuthorizationCode(clientId, code);
+          }
         }
-        JSONObject accessTokens = oauthDB.getJSONObject(clientId).getJSONObject("accessTokens");
-        for(String accessTokenId : JSONObject.getNames(accessTokens)){
-          OAuth2Code code = converter.convertToObject(accessTokens.getJSONObject(accessTokenId).toString(), OAuth2Code.class);
-          code.setValue(accessTokenId);
-          code.setClient(client);
-          code.setType(CodeType.ACCESS_TOKEN);
-          registerAccessToken(clientId, code);
+        if (oauthDB.getJSONObject(clientId).has("accessTokens")) {
+          JSONObject accessTokens = oauthDB.getJSONObject(clientId).getJSONObject("accessTokens");
+          for(String accessTokenId : JSONObject.getNames(accessTokens)){
+            OAuth2Code code = converter.convertToObject(accessTokens.getJSONObject(accessTokenId).toString(), OAuth2Code.class);
+            code.setValue(accessTokenId);
+            code.setClient(client);
+            code.setType(CodeType.ACCESS_TOKEN);
+            registerAccessToken(clientId, code);
+          }
         }
-        
       } catch (JSONException je) {
         throw new ProtocolException(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, je.getMessage(), je);
       }
