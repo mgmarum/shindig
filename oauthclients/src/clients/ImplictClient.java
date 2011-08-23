066eb5a6e9402ee76dbf1d1dc26ccaf801c83135
woodstae <woodstae@gmail.com>
2011-08-23 12:30:16 -0400

    Servlet for exercising implicit flow

---
+++ oauthclients/src/clients/ImplictClient.java
@@ -0,0 +1,20 @@
+package clients;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Performs the Auth 2.0 implicit grant flow.
+ */
+public class ImplictClient extends HttpServlet {
+
+	private static final long serialVersionUID = -629835685914414480L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+}
